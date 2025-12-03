000a05a22828246b5964f99a8b9ddb0053714d44
Aye Chan Han <boateng.ach27@gmail.com>
2025-12-03 18:21:56 +0800

    Use radomuser.me to fetch random user and display on UI

--- src/app/(root)/about/page.tsx
+++ src/app/(root)/about/page.tsx
@@ -1,7 +1,21 @@
-import React from "react";
+const Page = async () => {
+  const response = await fetch("https://randomuser.me/api/");
+  if (!response.ok) {
+    throw new Error("Failed to fetch user data");
+  }
+  const data = await response.json();
+  const user = data.results[0];
 
-const Page = () => {
-  return <div><h1>About Page</h1></div>;
+  return (
+    <div>
+      <h1>
+        {user.name.first} {user.name.last}
+      </h1>
+      <p>Email: {user.email}</p>
+      <p>Gender: {user.gender}</p>
+      <img src={user.picture.large} alt="User Picture" />
+    </div>
+  );
 };
 
 export default Page;
